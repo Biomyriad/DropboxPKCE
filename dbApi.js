b498e1d49258325e5a17d28aa0554422ed22e291
Biomyriad <biomyriad@gmail.com>
2024-08-27 04:42:13 -0700

    currently working DB Auth

--- dbApi.js
+++ dbApi.js
@@ -1,64 +1,72 @@
-console.log("FILEEE")
 class DbApi {
 
   // if url had code in its params then we prob
   // reconnecting so get logged in
   //eg. /1.1.1.1/?code=xxxxx
   constructor() {
-console.log("consttttt")
-    this.REDIRECT_URI = 'http://localhost:7700/';
+    console.log("cont")
+    this.REDIRECT_URI = 'http://localhost:5500/';
     this.CLIENT_ID = 'h7bzrn3vyfa3m2r';
     this.dbx = null
     this.dbxAuth = new Dropbox.DropboxAuth({
-      clientId: this.CLIENT_ID,
-      refreshToken: localStorage.getItem('refresh_token'),
-      accessToken: localStorage.getItem('access_token')
+      clientId: 'h7bzrn3vyfa3m2r',
+      refreshToken: localStorage.getItem('db_refresh_token'),
+      accessToken: localStorage.getItem('db_access_token'),
+      accessTokenExpiresAt: localStorage.getItem("db_expires_at")
     });
 
-
-    if (hasRedirectedFromAuth()) {
+    if (this.hasRedirectedFromAuth()) {
       this.dbxAuth.setCodeVerifier(sessionStorage.getItem('codeVerifier'));
-      this.dbxAuth.getAccessTokenFromCode(this.REDIRECT_URI, getCodeFromUrl())
+      this.dbxAuth.getAccessTokenFromCode(this.REDIRECT_URI, this.getCodeFromUrl())
         .then((response) => {
-          localStorage.setItem("refresh_token", response.result.refresh_token)
-          localStorage.setItem("access_token", response.result.access_token)
+          console.log(response)
+          localStorage.setItem("db_refresh_token", response.result.refresh_token)
+          localStorage.setItem("db_access_token", response.result.access_token)
           this.dbxAuth.setRefreshToken(response.result.refresh_token)
           this.dbxAuth.setAccessToken(response.result.access_token);
+          window.location.href = this.REDIRECT_URI
         })
         .catch((error) => {
           console.error(error.error || error);
         });
     } else {
-      login()
+      this.login()
     }
+  }
+
+  listFiles() { // EXAMPLE
+    var r = dbx.dbx.filesListFolder({path: ''}) // returns promise
+    r.then( text => {renderItems(text.result.entries); console.log(text.result.entries)})
+    .catch(e => console.log(e))
   }
 
   loginDropbox() {
     this.dbxAuth.getAuthenticationUrl(this.REDIRECT_URI, undefined, 'code', 'offline', undefined, undefined, true)
       .then(authUrl => {
-        localStorage.setItem("codeVerifier", this.dbxAuth.codeVerifier);
+        sessionStorage.setItem("codeVerifier", this.dbxAuth.codeVerifier);
         window.location.href = authUrl;
       })
       .catch((error) => console.error(error));
   }
 
-
-  listFiles() {
-
-    var r = this.dbx.filesListFolder({ path: '' })
-    renderItems(r)
-  }
-
   login() {
     this.dbxAuth.refreshAccessToken()
       .then((response) => {
+
         this.dbx = new Dropbox.Dropbox({
           auth: this.dbxAuth
-        });
+        })
+        
+        if(this.dbx.auth.accessToken !== null) localStorage.setItem("db_access_token", this.dbx.auth.accessToken)
+        if(this.dbx.auth.refreshToken !== localStorage.getItem('db_refresh_token')) {
+          localStorage.setItem("db_refresh_token", this.dbx.auth.refreshToken)
+          console.log("NEW RefreshToken!")
+        }
+        
       })
       .catch((error) => {
         console.error(error.error || error);
-        loginDropbox()
+        this.loginDropbox()
       })
   }
 
@@ -66,7 +74,7 @@ console.log("consttttt")
   // If the user was just redirected from authenticating, the urls hash will
   // contain the access token.
   hasRedirectedFromAuth() {
-    return !!getCodeFromUrl();
+    return !!this.getCodeFromUrl();
   }
 
   // Parses the url and gets the access token if it is in the urls hash
@@ -77,4 +85,44 @@ console.log("consttttt")
 
 }
 
-export { DbApi}
+(function (window) {
+  window.utils = {
+    parseQueryString(str) {
+      const ret = Object.create(null);
+
+      if (typeof str !== 'string') {
+        return ret;
+      }
+
+      str = str.trim().replace(/^(\?|#|&)/, '');
+
+      if (!str) {
+        return ret;
+      }
+
+      str.split('&').forEach((param) => {
+        const parts = param.replace(/\+/g, ' ').split('=');
+        // Firefox (pre 40) decodes `%3D` to `=`
+        // https://github.com/sindresorhus/query-string/pull/37
+        let key = parts.shift();
+        let val = parts.length > 0 ? parts.join('=') : undefined;
+
+        key = decodeURIComponent(key);
+
+        // missing `=` should be `null`:
+        // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+        val = val === undefined ? null : decodeURIComponent(val);
+
+        if (ret[key] === undefined) {
+          ret[key] = val;
+        } else if (Array.isArray(ret[key])) {
+          ret[key].push(val);
+        } else {
+          ret[key] = [ret[key], val];
+        }
+      });
+
+      return ret;
+    },
+  };
+}(window));
